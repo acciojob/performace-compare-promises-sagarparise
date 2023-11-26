0efd540c8eb4e90d996067731984b177640d36f2
sagarparise <141607123+sagarparise@users.noreply.github.com>
2023-11-26 21:35:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,3 +13,51 @@ const apiUrls = [
 ];
 
 // You can write your code here
+
+
+        async function fetchData(url) {
+            const startTime = Date.now();
+            const response = await fetch(url);
+            const endTime = Date.now();
+            const timeTaken = endTime - startTime;
+            return { url, timeTaken };
+        }
+
+        async function fetchDataWithPromiseAll(urls) {
+            const promises = urls.map(url => fetchData(url));
+            const results = await Promise.all(promises);
+            return results;
+        }
+
+        async function fetchDataWithPromiseAny(urls) {
+            const promises = urls.map(url => fetchData(url));
+            const result = await Promise.any(promises);
+            return result;
+        }
+
+        async function displayResults() {
+            try {
+                // Measure time taken for Promise.all
+                const promiseAllStartTime = Date.now();
+                const allResults = await fetchDataWithPromiseAll(apiUrls);
+                const promiseAllEndTime = Date.now();
+                const promiseAllTimeTaken = promiseAllEndTime - promiseAllStartTime;
+
+                // Display results for Promise.all
+                document.getElementById('output-all').innerText = `${promiseAllTimeTaken} ms`;
+
+                // Measure time taken for Promise.any
+                const promiseAnyStartTime = Date.now();
+                const anyResult = await fetchDataWithPromiseAny(apiUrls);
+                const promiseAnyEndTime = Date.now();
+                const promiseAnyTimeTaken = promiseAnyEndTime - promiseAnyStartTime;
+
+                // Display results for Promise.any
+                document.getElementById('output-any').innerText = `${promiseAnyTimeTaken} ms`;
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        }
+
+        // Call the function to start the process
+        displayResults();
